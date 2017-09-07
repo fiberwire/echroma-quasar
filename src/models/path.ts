e90c55c44ea40de33984fc75fc4d2c73b8806f2d
fiberwire <andrew.ramos92@gmail.com>
2017-09-07 10:42:01 -0400

    added width and height to painting

--- src/models/path.ts
+++ src/models/path.ts
@@ -1,4 +1,5 @@
 import { Gene } from 'enome';
+import * as _ from 'lodash';
 
 export class Path {
 
@@ -34,29 +35,53 @@ export class Path {
     return new Path(`${this.d} ${d}`);
   }
 
+  public circle(cx: number, cy: number, r: number): Path {
+    const d = `M ${cx - r}, ${cy} A ${r}, ${r} 0 1, 0 ${r * 2}, 0 A ${r}, ${r} 0 1, 0 ${-r * 2}, 0 `;
+    return new Path(`${this.d} ${d}`);
+  }
+
   public close(): Path {
     const d = `Z`;
     return new Path(`${this.d} ${d}`);
   }
 
   public random(
-    x1: number, y1: number,
-    x2: number, y2: number,
-    rx: number, ry: number,
-    rotation: number, large: number,
-    sweep: number, x: number,
-    y: number, close: boolean
+    o: {
+      length: number,
+      x1: number, y1: number,
+      x2: number, y2: number,
+      rx: number, ry: number,
+      cx: number, cy: number, r: number,
+      rotation: number, large: number,
+      sweep: number, x: number,
+      y: number, close: boolean
+    }
   ): Path {
-    const g = new Gene();
-    const moved = this.moveTo(x, y);
+    const moved = this.moveTo(o.x, o.y);
 
-    const path = g.element([
-      moved.curveTo(x1, y1, x2, y2, x, y),
-      moved.shortCurve(x2, y2, x, y),
-      moved.quadraticCurve(x1, y1, x, y),
-      moved.shortQuadratic(x, y),
-      moved.arc(rx, ry, rotation, large, sweep, x, y)
-    ]);
+    const {
+      length, x1, y1,
+      x2, y2, rx, ry,
+      cx, cy, r,
+      rotation, large, sweep,
+      x, y, close
+    } = o;
+
+    const paths: Path[] = _.range(length)
+      .map(i => {
+        const g = new Gene();
+
+        return g.element([
+          moved.curveTo(x1, y1, x2, y2, x, y),
+          moved.shortCurve(x2, y2, x, y),
+          moved.quadraticCurve(x1, y1, x, y),
+          moved.shortQuadratic(x, y),
+          moved.arc(rx, ry, rotation, large, sweep, x, y),
+          moved.circle(cx, cy, r)
+        ]);
+      });
+
+    const path = paths.reduce((p, c) => new Path(`${p.d} ${c.d}`));
 
     if (close) { return path.close(); }
     else { return path; }
