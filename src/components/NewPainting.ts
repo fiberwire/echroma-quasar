0f1c7d47c1d554ae2b2c1436240c5d1f99247481
fiberwire <andrew.ramos92@gmail.com>
2017-09-08 01:39:51 -0400

    added specimen to PaintingComponent data
    along with a test options

--- src/components/NewPainting.ts
+++ src/components/NewPainting.ts
@@ -1,5 +1,4 @@
-import Vue from 'vue';
-import Component from 'vue-class-component';
+import { Vue, Component, Prop } from 'vue-property-decorator';
 
 import {
   QIcon,
@@ -13,6 +12,11 @@ import {
   QToolbarTitle,
 } from 'quasar';
 
+import PaintingComponent from './Painting';
+import { PaintingSpecimen } from '../evolution/painting-specimen';
+import { PaintingGenOptions } from '../evolution/painting-gen-options';
+import { Genome } from 'enome';
+
 @Component({
   name: 'Home',
   components: {
@@ -25,8 +29,42 @@ import {
     QItemSide,
     QToolbar,
     QToolbarTitle,
+    PaintingComponent,
   }
 })
 export default class NewPainting extends Vue {
 
+
+  data() {
+    const specimen = new PaintingSpecimen(new Genome({
+      genomeLength: 1000,
+      geneLength: 3,
+      width: 1920,
+      height: 1080,
+      minX: 0,
+      maxX: 1920,
+      minY: 0,
+      maxY: 1080,
+      minPaths: 10,
+      maxPaths: 1000,
+      minLength: 1,
+      maxLength: 3,
+      minXRadius: 5,
+      maxXRadius: 50,
+      minYRadius: 5,
+      maxYRadius: 50,
+      minRotation: 0,
+      maxRotation: 360,
+      minLarge: 0,
+      maxLarge: 1,
+      minSweep: 0,
+      maxSweep: 1,
+      minRadius: 5,
+      maxRadius: 50
+    }));
+
+    return {
+      specimen
+    };
+  }
 }
